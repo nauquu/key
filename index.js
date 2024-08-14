e4958dae1e5c73c41bbd4a10d8a1c745bfb295e8
Vũ Quân <suquan357@gmail.com>
2024-08-14 22:13:12 +0700

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const fs = require('fs');
 const app = express();
 
-const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const data = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
 
 app.get('/api', (req, res) => {
     res.json(data);
