5932ac9d0c50de80c4222166c5351f0d028d1c9f
Vũ Quân <suquan357@gmail.com>
2024-08-14 22:16:16 +0700

    .

--- index.js
+++ index.js
@@ -2,7 +2,26 @@ import express from 'express';
 const fs = require('fs');
 const app = express();
 
-const data = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
+// const data = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
+const data = {
+    "hexn": {
+        "freeTrial":[
+            {
+                "key": "123",
+                "isExpied": false,
+                "activeTime": "",
+                "owner": "@"
+            }
+        ],
+        "paid":[
+            {
+                "key": "123",
+                "activeTime": "",
+                "owner": "@"
+            }
+        ]
+    }
+}
 
 app.get('/api', (req, res) => {
     res.json(data);
