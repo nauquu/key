ce26df52b1fc0ef3565837940f58237ee0082908
Vũ Quân <suquan357@gmail.com>
2024-08-14 22:06:51 +0700

    .

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+
+app.get('/api', (req, res) => {
+    res.json(data);
+});
+
+module.exports = app;
